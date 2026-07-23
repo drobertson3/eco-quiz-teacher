8e206219fba72224fbc992e2951fab29476c9082
drobertson3 <d.robertson@scas.nsw.edu.au>
2026-07-24 08:48:00 +1000

    Add files via upload

---
+++ syllabus.js
@@ -0,0 +1,6 @@
+const SYLLABUS = {
+  "Global Economy": ["Introduction to the global economy","Trade, financial flows and foreign investment","Protection","Globalisation and economic development"],
+  "Australia & Global Economy": ["Australia's trade and financial flows","Exchange rates","Protection in Australia"],
+  "Economic Issues": ["Economic growth","Unemployment","Inflation","External stability","Distribution of income and wealth","Environmental sustainability"],
+  "Economic Policies": ["Objectives of economic policy","Fiscal policy","Monetary policy","Microeconomic and labour market reform","Limitations of economic policy"]
+};
